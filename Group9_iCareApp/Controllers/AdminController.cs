5c860b2c4733fe1fc70cf0b690168473c98086f8
Ckiner <christiankiner21@icloud.com>
2024-11-05 15:04:31 -0600

    Created the admin controller and skeleton for views

---
+++ Group9_iCareApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group9_iCareApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Manage()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
