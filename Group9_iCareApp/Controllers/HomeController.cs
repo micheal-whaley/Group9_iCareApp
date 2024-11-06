271a629a6cfa7ff04a34e1deacf32ee044654782
KhuramC <khuramchoudhry123@gmail.com>
2024-11-06 02:53:55 -0600

    Removed unused controllers. Added help/about us page.

--- Group9_iCareApp/Controllers/HomeController.cs
+++ Group9_iCareApp/Controllers/HomeController.cs
@@ -6,44 +6,20 @@ namespace Group9_iCareApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly iCAREDBContext _usercontext;
-
-        public HomeController(ILogger<HomeController> logger, iCAREDBContext usercontext)
-        {
-            _logger = logger;
-            _usercontext = usercontext;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult RegisterPage()
+        public IActionResult AboutUs()
         {
             return View();
         }
 
-        public IActionResult LoginPage()
+        public IActionResult Help()
         {
             return View();
-        }
-
-        public IActionResult LoginForm(iCAREUser model)
-        {
-            // Login for administrators
-            if (model.Id == null) // doesnt exist yet
-            {
-                _usercontext.iCAREUsers.Add(model);
-            }
-            else
-            {
-               // _context.Expenses.Update(model);
-            }
-            _usercontext.SaveChanges();
-
-            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
