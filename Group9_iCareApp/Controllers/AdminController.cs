b0f3fd4023befd1ab92c2ce99af15ddf5637e5ba
Ckiner <christiankiner21@icloud.com>
2024-11-07 14:41:29 -0600

    Created manage worker with functioning delete.
    FK Constraint removed from UserAccount in iCAREWorker table

--- Group9_iCareApp/Controllers/AdminController.cs
+++ Group9_iCareApp/Controllers/AdminController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Group9_iCareApp.Models;
 
 namespace Group9_iCareApp.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly iCAREDBContext context;
+
+        public AdminController(iCAREDBContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,6 +26,18 @@ namespace Group9_iCareApp.Controllers
         public IActionResult Create()
         {
             return Redirect("~/Identity/Account/Register");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int workerId)
+        {
+            var worker = context.iCAREWorkers.Find(workerId);
+            var user = context.iCAREUsers.Find(worker.UserAccount);
+
+            context.iCAREWorkers.Remove(worker);
+            context.iCAREUsers.Remove(user);
+            context.SaveChanges();
+            return RedirectToAction("Manage");
         }
 
         public IActionResult Success()
